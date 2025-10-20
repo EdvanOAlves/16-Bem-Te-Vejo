9526bc4f294347802646224ae8e3669f49d23891
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2025-10-20 09:58:21 -0300

    style: responsivity on tablet landing page

--- js/speciesSearch.js
+++ js/speciesSearch.js
@@ -8,8 +8,6 @@ TODO:
 
     Implementar busca por pesquisa
 
-    Implementar voltar um nível na navegação de busca
-
 */
 let nivel = 0 //Contagem de páginas, quando chega em 3 está em família, exibindo a lista de espécies
 
@@ -17,11 +15,14 @@ const buscaLista = document.getElementById('lista');
 let rota = [];
 let enderecoAtual;
 
+console.log(rota);
+
 const buttonVoltar = document.getElementById('returnButton');
+
 buttonVoltar.addEventListener('click', () => returnPage())
 
 
-async function getChildren(id){
+async function getChildren(id) {
     const endPoint = `https://api.inaturalist.org/v1/taxa?parent_id=${id}&per_page=100`
     const response = await fetch(endPoint);
     const data = await response.json();
@@ -29,7 +30,7 @@ async function getChildren(id){
 }
 
 
-function carregarCard(item){
+function carregarCard(item) {
     const itemCard = document.createElement('li');
     const itemNome = document.createElement('h2');
     const itemImg = document.createElement('img');
@@ -37,71 +38,74 @@ function carregarCard(item){
 
     itemNome.textContent = item.name;
 
-    if (item.default_photo){
+    if (item.default_photo) {
         itemImg.src = item.default_photo.medium_url;
     }
-    else{ 
+    else {
         itemImg.alt = 'Falha no carregamento de imagem';
     }
 
     itemCard.classList.add('busca-card');
-    
+
     itemCard.appendChild(itemNome);
     itemCard.appendChild(itemImg);
-    
+
 
     buscaLista.appendChild(itemCard);
-    
-    if (nivel >= 3 && item.rank == 'species'){   // Caso estejamos lidando com espécie
-    itemCard.addEventListener('click', () => window.location.href = `info-ave.html?especie=${item.name}`); 
+
+    if (nivel >= 3 && item.rank == 'species') {   // Caso estejamos lidando com espécie
+        itemCard.addEventListener('click', () => window.location.href = `info-ave.html?especie=${item.name}`);
     }
 
     else // Caso contrário
-    itemCard.addEventListener('click', () => loadChosenPage(id));
-   
+        itemCard.addEventListener('click', () => loadChosenPage(id));
+
 }
 
 
 
-async function loadChosenPage(id){
+async function loadChosenPage(id) {
+
+
     const pageData = await getChildren(id);
     buscaLista.replaceChildren();
 
     pageData.results.forEach(carregarCard);
 
-    if(enderecoAtual)
+    if (enderecoAtual)
         rota.push(enderecoAtual);
     enderecoAtual = id;
-    if (rota.length >= 1){
+    if (rota.length >= 1) {
         buttonVoltar.classList.remove('hidden')
     }
     
     
-    nivel+=1;
+    nivel += 1;
 }
 
-async function loadReturningPage(id){
+async function loadReturningPage(id) {
     const pageData = await getChildren(id);
     buscaLista.replaceChildren();
-
+    
     pageData.results.forEach(carregarCard);
-
+    
     nivel--;
-    console.log('nivel é '+ nivel)
-    if (nivel == 1){
+    console.log('nivel é ' + nivel)
+    if (nivel == 1) {
         buttonVoltar.classList.add('hidden');
     }
 }
 
 
 
-async function returnPage(){
+async function returnPage() {
     let id = rota.pop();
     enderecoAtual = id;
     loadReturningPage(id);
 }
 
 console.log('hello world!');
+
 loadChosenPage(3); //id de aves é 3
 // loadFamiliesPage();
 // loadSpeciesPage();
