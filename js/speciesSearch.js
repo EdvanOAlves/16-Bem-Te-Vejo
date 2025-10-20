ba3cc82d8d82a5b9251fd14a2cd31a938c3184d5
unknown <edvan.aoliveira324@gmail.com>
2025-10-20 03:10:57 -0300

    feat: adds fetch for families and species

--- js/speciesSearch.js
+++ js/speciesSearch.js
@@ -3,6 +3,12 @@
 TODO:
     O carregamento é limitado a 199 itens por requisição, deixei o 199 mas é uma solução temporária,
     estudar depois como fazer a requisição em partes menores para cobrir todo o conteúdo sem sobrecarregar
+
+    Atualizar o texto da etapa que o usuário se encontra
+
+    Implementar interação, porque até o momento só tem fetch sem navegação sequer para acessar os resultados
+
+    Implementar busca por pesquis
 */
 
 
@@ -16,19 +22,38 @@ async function getOrders(){
     return data;
 }
 
+async function getFamilies(){
+    //ROTA EXEMPLO DE PASSERIFORMES, alterar depois com o target selecionado pelo usuário
+    const endPoint = `https://api.inaturalist.org/v1/taxa?parent_id=7251&rank=family&per_page=199`;
+    const response = await fetch(endPoint);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+async function getSpecies(){
+    //ROTA EXEMPLO DE PASSERIFORMES, alterar depois com o target selecionado pelo usuário
+    const endPoint = `https://api.inaturalist.org/v1/taxa?parent_id=7823`;
+    const response = await fetch(endPoint);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+
 function carregarCard(item){
-    const ordemCard = document.createElement('li');
-    const ordemNome = document.createElement('h2');
-    const ordemImg = document.createElement('img');
+    const itemCard = document.createElement('li');
+    const itemNome = document.createElement('h2');
+    const itemImg = document.createElement('img');
 
-    ordemNome.textContent = item.name;
-    ordemImg.src = item.default_photo.medium_url;
-    ordemCard.classList.add('busca-card')
+    itemNome.textContent = item.name;
+    itemImg.src = item.default_photo.medium_url;
+    itemCard.classList.add('busca-card')
 
-    ordemCard.appendChild(ordemNome);
-    ordemCard.appendChild(ordemImg);
+    itemCard.appendChild(itemNome);
+    itemCard.appendChild(itemImg);
 
-    buscaLista.appendChild(ordemCard);
+    buscaLista.appendChild(itemCard);
 }
 
 async function loadOrdersPage(){
@@ -37,5 +62,21 @@ async function loadOrdersPage(){
     ordersData.results.forEach(carregarCard);
 }
 
+async function loadFamiliesPage(order){
+    const familiesData = await getFamilies(order);
+    buscaLista.replaceChildren();
+
+    familiesData.results.forEach(carregarCard);
+}
+
+async function loadSpeciesPage(family){
+    const speciesData = await getSpecies(family);
+    buscaLista.replaceChildren();
+
+    speciesData.results.forEach(carregarCard);
+}
+
 console.log('hello world!');
-loadOrdersPage();
+// loadOrdersPage();
+// loadFamiliesPage();
+loadSpeciesPage();
